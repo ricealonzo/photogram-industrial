85b424f9604c515834b8e4657a2d54f7b050726e
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2025-05-07 20:45:10 +0000

    updated devise username input

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
   root "photos#index"
-  get "/users/:id" => "users#show", as: :user
-end
-
+  # get "/users/:id" => "users#show", as: :user
+  # get "/:username" => "users#show", as: :user
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  devise_for :users
-  get "/:username" => "users#show", as: :user
 
+  devise_for :users
+  
+  
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
+  
   # Defines the root path route ("/")
   # Defines the root path route ("/") -
+end
