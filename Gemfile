6743822e6ba8bcce49e0866e381da66041cfad14
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2025-05-14 17:26:51 +0000

    almost finished

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ group :development do
 end
 
 # Additional gems for AppDev 2
+gem "active_support"
 gem "active_link_to"
 gem "awesome_print"
 gem "dotenv"
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/discover" => "users#discover", as: :discover
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
