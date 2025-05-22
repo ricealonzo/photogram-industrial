265af4c2f0c06de40181316d0be6ff0008f8c615
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2025-05-22 18:41:12 +0000

    COMPLETED

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,7 @@ class User < ApplicationRecord
          has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
 
          has_many :pending_sent_follow_requests, -> { where(status: "pending") }, foreign_key: :sender_id, class_name: "FollowRequest"
+         has_many :pending_received_follow_requests, -> { where(status: "pending") }, foreign_key: :recipient_id, class_name: "FollowRequest"
          
          has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
          
@@ -58,7 +59,7 @@ class User < ApplicationRecord
        
          has_many :followers, through: :accepted_received_follow_requests, source: :sender
          
-         has_many :pending, through: :pending_sent_follow_requests,  source: :recipient
+         has_many :pending, through: :pending_received_follow_requests,  source: :recipient
 
        
          has_many :feed, through: :leaders, source: :own_photos
